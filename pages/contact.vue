6d02724e8817d66c998461fe62e114efb86182d3
alexander <alexander@MacBook-Pro-alexander.local>
2020-04-30 07:56:46 +0300

    Fix container padding and contact email link

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,7 +4,7 @@
       nuxt-link(to="/").link ← Go Back
       h1.title.title--size-xlarge.title--weight-normal.title--align-center
         a(href="mailto:alexander.katkov.h@icloud.com").mail-link alexander.katkov.h@icloud.com
-      .title.title--level-2 or wtire to my #[a(href="//vk.com/7architect").link VK] 
+      .title.title--level-2 or wtire to my #[a(href="//vk.com/7architect").link VK]
 </template>
 
 <style lang="stylus">
@@ -14,4 +14,6 @@
     color: $color.white
     text-decoration none
     border-bottom: 1px dashed lightness($color.white, 40%)
+    word-break break-word
+    text-align center
 </style>
