0e3db7fb0bf2dfc5e1d7c69717d179434a62944d
alexander <alexander@MacBook-Pro-alexander.local>
2020-05-03 02:28:17 +0300

    Add Yandex verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Timeline | Alexander Katkov' }
+      { hid: 'description', name: 'description', content: 'Timeline | Alexander Katkov' },
+      { name: 'yandex-verification', content: '4d326bd5bf7a7a00' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
